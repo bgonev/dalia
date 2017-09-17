69750ada16fd09a8cf806e5e99d95ea937746fb6
Boris Gonev <bgonev@gmail.com>
2017-09-17 22:47:56 +0200

    Fix

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,12 +5,14 @@ include wget
 include ntp
 include nfsclient
 include webcontent::ssl
+include webcontent
 }
 
 node web2.domain.com {
 include wget
 include ntp
 include nfsclient
+include webcontent
 }
 
 node nfsserver1.domain.com {
@@ -30,6 +32,7 @@ node sql1.domain.com {
 include wget
 include ntp
 include nfsclient
+include postgremaster
 }
 
 node sql2.domain.com {
